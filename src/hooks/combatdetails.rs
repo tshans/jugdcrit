37ffdef15537bc78573e3f05e10b5674cbd89535
tshans <149139896+tshans@users.noreply.github.com>
2026-03-31 22:02:52 -0600

    Consolidated jugdral_crits formulas into one entry.

--- src/hooks/combatdetails.rs
+++ src/hooks/combatdetails.rs
@@ -9,61 +9,14 @@ use crate::engagelib::battle::BattleParamStaticFields;
 use crate::engagelib::battle::BattleSceneResult;
 
 
-// Set CRITICAL_FACTOR equal to 1 or 2 for the corresponding Critical Formula Option.
-// This is used to apply Skill changes to damage for criticals.
-
+// CRITICAL_FACTOR is a constant used to determine how "true damage" skills are affected by
+// crits. A value of 2 matches the Jugdral formula's attack doubling and is the default.
 // (The vanilla game uses the BattleInfo field value const int CriticalFactor = 3.)
-// pub const CRITICAL_FACTOR: i32 = 1;
 pub const CRITICAL_FACTOR: i32 = 2;
 
 
-// Critical Formula Option #1
-// Damage = [Max(2 * Attack - Defense, 0) + Add] * Scale
-
-// fn jugdral_critical(current: &BattleInfoSide) -> i32 {
-//     let reverse = match current.get_reverse() {
-//         Some(infoside) => infoside,
-//         None => panic!("No attack target for jugdral_critical function."),
-//     };
-//     let current_details = current.get_detail();
-//     let reverse_details = reverse.get_detail();
-
-//     let attack_param = current_details
-//         .get_attack()
-//         .expect("Attack BattleParam should be initialized.");
-//     let defense_param = reverse_details
-//         .get_defense()
-//         .expect("Defense BattleParam should be initialized.");
-//     let power_param = current_details
-//         .get_simple_power()
-//         .expect("SimplePower BattleParam should be initialized.");
-
-//     let statics = BattleParam::class()
-//         .get_static_fields::<BattleParamStaticFields>();
-//     let min = statics.mins.fields.deref();
-//     let max = statics.maxs.fields.deref();
-
-//     let attack = attack_param.get_result(current);
-//     let defense = defense_param.get_result(reverse);
-
-//     let add = power_param.fields.add;
-//     let scale = power_param.fields.scale;
-
-//     let kind = power_param.get_kind();
-
-//     let calc = CRITICAL_FACTOR * attack as i32 - defense as i32;
-//     let max_calc = calc.max(0);
-        
-//     let damage = ((max_calc as f32 + add) * scale)
-//         .clamp(min[kind as usize], max[kind as usize]) as i32;
-
-//     return damage
-// }
-
-
-// Critical Formula Option #2
-// Damage = Max{[2 * (Attack + Add) - Defense], 0} * Scale
-
+// Critical Formula
+// Damage = [Max(2 * Attack - Defense, 0) + Add * CRITICAL_FACTOR] * Scale
 pub fn jugdral_critical(current: &BattleInfoSide) -> i32 {
     let reverse = match current.get_reverse() {
         Some(infoside) => infoside,
@@ -95,11 +48,13 @@ pub fn jugdral_critical(current: &BattleInfoSide) -> i32 {
     let scale = power_param.fields.scale;
 
     let kind = power_param.get_kind();
-    
-    let calc = CRITICAL_FACTOR * (attack as i32 + add as i32) - defense as i32;
+
+    // Vanilla formula uses
+    // let calc = attack as i32 - defense as i32;
+    let calc = 2 * attack as i32 - defense as i32;
     let max_calc = calc.max(0);
 
-    let damage = (max_calc as f32 * scale)
+    let damage = ((max_calc + CRITICAL_FACTOR * add as i32) as f32 * scale)
         .clamp(min[kind as usize], max[kind as usize]) as i32;
     
     return damage
